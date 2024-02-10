405db389e54520768b44aa76ad0b3ea4fbbb1a64
Kirankmane <120165413+Kirankmane@users.noreply.github.com>
2024-02-10 23:18:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
   ami           = "ami-03f4878755434977f"
   instance_type = "t2.micro"
   tags = {
-    Name = "ExampleInstance"
+    Name = "New-Instance"
   }
 }
 
